383eb703cb0ce6ef16855020875b236897510598
Bojan Lozo <bojanlo@maestralsolutions.com>
2023-04-11 10:43:01 +0200

    Project scaffolding

---
+++ script.js
@@ -0,0 +1,27 @@
+// niz objekata koji predstavljaju oblast i ponudjene rijeci
+const pitanja = [
+    {
+      oblast: "Pogodite grad u Crnoj Gori",
+      ponudjeno: [
+        "podgorica",
+        "budva",
+        "bar",
+        "kolašin",
+        "danilovgrad",
+        "tivat",
+        "kotor"
+      ]
+    },
+    {
+      oblast: "Pogodite programski jezik",
+      ponudjeno:[
+        "javascript",
+        "php",
+        "python",
+        "cpp",
+        "csharp",
+        "kotlin"
+      ]
+    }
+  ];
+let abeceda = ['a','b','c','č','ć','d','dž','đ','e','f','g','h','i','j','k','l','lj','m','n','nj','o','p','r','s','š','t','u','v','z','ž'];
